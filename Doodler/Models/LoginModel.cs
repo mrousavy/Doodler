683934f4c820d2c1bb96b7e413c5187d669bcd72
mrousavy <mrousavy@donotreply.github.com>
2018-05-09 19:06:00 +0200

    Add Login View, Model and ViewModel

---
+++ Doodler/Models/LoginModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Authentication;
+using System.Threading.Tasks;
+
+namespace Doodler.Models
+{
+    public class LoginModel
+    {
+        public async Task<bool> TryLoginAsync(string email, string password)
+        {
+            using (var service = Statics.NewService())
+            {
+                try
+                {
+                    var user = await service.LoginAsync(email, password);
+                    return true;
+                } catch (InvalidCredentialException)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ Doodler/ViewModels/LoginViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+using Doodler.Implementation;
+using Doodler.Models;
+
+namespace Doodler.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        #region Properties
+        private string _email;
+        private string _password;
+
+        public string Email
+        {
+            get => _email;
+            set => Set(ref _email, value);
+        }
+        public string Password
+        {
+            get => _password;
+            set => Set(ref _password, value);
+        }
+        public ICommand LoginCommand { get;  }
+        public LoginModel Model { get; }
+        #endregion
+
+        public LoginViewModel()
+        {
+            LoginCommand = new RelayCommand(LoginAction);
+            Model = new LoginModel();
+        }
+
+        private async void LoginAction(object o)
+        {
+            await Model.TryLoginAsync(Email, Password);
+        }
+    }
+}
