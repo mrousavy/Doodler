6e59254681f6de276d6874c163d4d32f2ce6457f
mrousavy <mrousavy@donotreply.github.com>
2018-05-09 17:48:16 +0200

    Create custom exceptions

---
+++ DoodlerCore/InvalidCredentialsException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DoodlerCore
+{
+    public class InvalidCredentialsException : Exception
+    {
+        public string Email { get; set; }
+
+        public InvalidCredentialsException(string email) : base("Invalid email or password!")
+        {
+            Email = email;
+        }
+    }
+}
---
+++ DoodlerCore/PollNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DoodlerCore
+{
+    public class PollNotFoundException : Exception
+    {
+        public Guid Id { get; set; }
+
+        public PollNotFoundException(Guid id) : base($"The poll with the id {id} could not be found!")
+        {
+            Id = id;
+        }
+    }
+}
