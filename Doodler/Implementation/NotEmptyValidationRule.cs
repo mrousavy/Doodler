70578f378e18630df15e5f7d9704d3402b880409
mrousavy <mrousavy@donotreply.github.com>
2018-05-09 19:23:38 +0200

    Add a non-empty validation rule

---
+++ Doodler/Implementation/NotEmptyValidationRule.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Doodler.Implementation
+{
+    public class NotEmptyValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value is string s)
+                return new ValidationResult(!string.IsNullOrWhiteSpace(s), "Can't be empty!");
+            else
+                return new ValidationResult(false, "Please enter a valid string!");
+        }
+    }
+}
