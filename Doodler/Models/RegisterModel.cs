6f30e2e1bb69bb743673fe70b26d8b474292b3ed
mrousavy <mrousavy@donotreply.github.com>
2018-05-09 19:06:07 +0200

    Add Register View, Model and ViewModel

---
+++ Doodler/Models/RegisterModel.cs
@@ -0,0 +1,23 @@
+﻿using DoodlerCore;
+using System.Threading.Tasks;
+
+namespace Doodler.Models
+{
+    public class RegisterModel
+    {
+        public async Task<bool> TryRegisterAsync(string email, string username, string password)
+        {
+            using (var service = Statics.NewService())
+            {
+                try
+                {
+                    var user = await service.RegisterAsync(email, username, password);
+                    return true;
+                } catch (EmailExistsException)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ Doodler/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Input;
+using Doodler.Implementation;
+using Doodler.Models;
+
+namespace Doodler.ViewModels
+{
+    public class RegisterViewModel : ViewModelBase
+    {
+        #region Properties
+        private string _email;
+        private string _username;
+        private string _password;
+
+        public string Email
+        {
+            get => _email;
+            set => Set(ref _email, value);
+        }
+        public string Username
+        {
+            get => _username;
+            set => Set(ref _username, value);
+        }
+        public string Password
+        {
+            get => _password;
+            set => Set(ref _password, value);
+        }
+        public ICommand RegisterCommand { get; }
+        public RegisterModel Model { get; }
+        #endregion
+
+        public RegisterViewModel()
+        {
+            RegisterCommand = new RelayCommand(RegisterAction);
+            Model = new RegisterModel();
+        }
+
+        private async void RegisterAction(object o)
+        {
+            await Model.TryRegisterAsync(Email, Username, Password);
+        }
+    }
+}
