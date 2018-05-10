b51e48b83a2e1531a40ad82d808111da64143d96
mrousavy <mrousavy@donotreply.github.com>
2018-05-10 14:07:07 +0200

    Add an Error Dialog View

---
+++ Doodler/Views/ErrorDialogView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace Doodler.Views
+{
+    /// <summary>
+    /// Interaction logic for ErrorDialogView.xaml
+    /// </summary>
+    public partial class ErrorDialogView 
+    {
+        public ErrorDialogView()
+        {
+            InitializeComponent();
+            TextBlock.DataContext = this;
+        }
+
+        public string Text
+        {
+            get => (string) GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string),
+                typeof(ErrorDialogView), new FrameworkPropertyMetadata
+                {
+                    BindsTwoWayByDefault = true
+                });
+    }
+}
