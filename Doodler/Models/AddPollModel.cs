73844f39a8ca5db6ad12de9dba94b83560eb3199
mrousavy <mrousavy@donotreply.github.com>
2018-05-10 20:25:08 +0200

    Add AddPoll dialog

---
+++ Doodler/Models/AddPollModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DoodlerCore;
+
+namespace Doodler.Models
+{
+    public class AddPollModel
+    {
+        public async Task CreatePollAsync<TAnswer>(User creator, string title, DateTime endDate, IEnumerable<TAnswer> answers)
+        where TAnswer : Answer
+        {
+            using (var service = Statics.NewService())
+            {
+                await service.CreatePollAsync(creator, title, endDate, answers);
+            }
+        }
+    }
+}
---
+++ Doodler/ViewModels/AddPollViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Doodler.Implementation;
+using DoodlerCore;
+using System;
+using Doodler.Models;
+
+namespace Doodler.ViewModels
+{
+    public class AddPollViewModel : ViewModelBase
+    {
+        private string _title;
+        private User _user = Statics.CurrentUser;
+        private PollType _type;
+        private DateTime _endDate = DateTime.Now;
+
+        #region Properties
+        public AddPollModel Model { get; }
+        public DateTime EndDate
+        {
+            get => _endDate;
+            set => Set(ref _endDate, value);
+        }
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+
+        public User User
+        {
+            get => _user;
+            set => Set(ref _user, value);
+        }
+
+        public PollType Type
+        {
+            get => _type;
+            set => Set(ref _type, value);
+        }
+        #endregion
+
+        public AddPollViewModel()
+        {
+            Model = new AddPollModel();
+        }
+    }
+}
