756682ad20e6be1efb638093e2881887b1d5c0ae
mrousavy <mrousavy@donotreply.github.com>
2018-05-10 13:37:28 +0200

    Add Preferences

--- Doodler/App.xaml.cs
+++ Doodler/App.xaml.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace Doodler
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        private void AppExit(object sender, ExitEventArgs e)
+        {
+            Preferences.Save(Statics.Preferences);
+        }
     }
 }
--- Doodler/Statics.cs
+++ Doodler/Statics.cs
@@ -1,12 +1,20 @@
-﻿using DoodlerCore;
+﻿using System.Threading.Tasks;
+using DoodlerCore;
 
 namespace Doodler
 {
     public static class Statics
     {
-        public static IDataService NewService()
-        {
-            return null;
-        }
+        public static Preferences Preferences { get; } = Preferences.Load();
+
+        public static string Database => Preferences.Database;
+        public static string Server => Preferences.Server;
+        public static string Username => Preferences.Username;
+
+        // TODO: BAD RAW PASSWORD STORING
+        public static string Password => Preferences.Password;
+
+        public static IDataService NewService() => new DataService(Database, Server, Username, Password);
+        public static Task CreateDatabaseAsync(IDataService service) => Task.Run(() => service.Create());
     }
 }
