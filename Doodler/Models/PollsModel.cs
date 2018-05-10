f8eab7d0a025c6499c3db4aea897ee3375f30e2c
mrousavy <mrousavy@donotreply.github.com>
2018-05-10 18:37:42 +0200

    Create PollsView

---
+++ Doodler/Models/PollsModel.cs
@@ -0,0 +1,17 @@
+﻿using DoodlerCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Doodler.Models
+{
+    public class PollsModel
+    {
+        public async Task<IEnumerable<Poll>> GetAllPollsAsync()
+        {
+            using (var service = Statics.NewService())
+            {
+                return await service.GetAllPollsAsync();
+            }
+        }
+    }
+}
---
+++ Doodler/ViewModels/PollsViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Doodler.Implementation;
+using DoodlerCore;
+using System.Collections.ObjectModel;
+
+namespace Doodler.ViewModels
+{
+    public class PollsViewModel : ViewModelBase
+    {
+        #region Properties
+
+        private int _tileColumns;
+
+        private ObservableCollection<Poll> _polls;
+
+        public ObservableCollection<Poll> Polls
+        {
+            get => _polls;
+            set => Set(ref _polls, value);
+        }
+        public int TileColumns
+        {
+            get => _tileColumns;
+            set => Set(ref _tileColumns, value);
+        }
+        #endregion
+    }
+}
