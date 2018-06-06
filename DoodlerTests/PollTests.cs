4c651949a995fd611d54a639b3765f9d177f9cc6
mrousavy <mrousavy@donotreply.github.com>
2018-06-06 20:38:07 +0200

    extract user tests to separate class

--- DoodlerTests/PollTests.cs
+++ DoodlerTests/PollTests.cs
@@ -1,6 +1,5 @@
 ﻿using DoodlerCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
@@ -10,12 +9,10 @@ namespace DoodlerTests
     public class PollTests : IDisposable
     {
         public User CurrentUser { get; set; }
-        public Random Random { get; set; }
 
         public PollTests()
         {
             CurrentUser = Statics.LoginUser().GetAwaiter().GetResult();
-            Random = new Random();
         }
 
         public void Dispose()
@@ -29,9 +26,9 @@ namespace DoodlerTests
         }
 
         [Theory]
-        [InlineData("Test: Is [person] nice?")]
-        [InlineData("Test: Is the weather nice today?")]
-        [InlineData("Test: Is Doodler well implemented?")]
+        [InlineData("Test: Is [person] nice?", "yes", "kinda", "no, not really")]
+        [InlineData("Test: Is the weather nice today?", "no", "it's london what do you expect", "yes")]
+        [InlineData("Test: Is Doodler well implemented?", "yes", "of course", "perfect as always")]
         public async Task TestCreateTextPoll(string title, params string[] answers)
         {
             Poll poll;
@@ -50,30 +47,6 @@ namespace DoodlerTests
 
                 Assert.NotNull(foundPoll);
                 Assert.Equal(poll.Id, foundPoll.Id);
-            }
-        }
-
-        [Theory]
-        [InlineData("mrousavy", "mrousavy@mail.com")]
-        [InlineData("dmaniak", "dmaniak@mail.com")]
-        [InlineData("mceylan", "mceylan@mail.com")]
-        public async Task TestCreateUser(string name, string email)
-        {
-            User user;
-            // Create user
-            using (var service = Statics.NewService())
-            {
-                user = await service.RegisterAsync(email, name, Random.Next(1111111, 9999999).ToString());
-                await service.SaveAsync();
-            }
-
-            // Find user
-            using (var service = Statics.NewService())
-            {
-                var foundUser = await service.GetUserByIdAsync(user.Id);
-
-                Assert.NotNull(foundUser);
-                Assert.Equal(user.Id, foundUser.Id);
             }
         }
     }
---
+++ DoodlerTests/UserTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DoodlerCore;
+using Xunit;
+
+namespace DoodlerTests
+{
+    public class UserTests : IDisposable
+    {
+        public Random Random { get; set; }
+
+        public UserTests()
+        {
+            Random = new Random();
+        }
+
+        public void Dispose()
+        {
+        }
+
+        [Fact]
+        public void TestFact()
+        {
+            Assert.True(true);
+        }
+
+        [Theory]
+        [InlineData("mrousavy", "mrousavy@mail.com")]
+        [InlineData("dmaniak", "dmaniak@mail.com")]
+        [InlineData("mceylan", "mceylan@mail.com")]
+        public async Task TestCreateUser(string name, string email)
+        {
+            User user;
+            // Create user
+            using (var service = Statics.NewService())
+            {
+                user = await service.RegisterAsync(email, name, Random.Next(1111111, 9999999).ToString());
+                await service.SaveAsync();
+            }
+
+            // Find user
+            using (var service = Statics.NewService())
+            {
+                var foundUser = await service.GetUserByIdAsync(user.Id);
+
+                Assert.NotNull(foundUser);
+                Assert.Equal(user.Id, foundUser.Id);
+            }
+        }
+    }
+}
