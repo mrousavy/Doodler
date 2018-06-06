33ae160aad9d6bbb9347e68a0a6f768cbb675b9b
Dario Bruckner TGM <dbruckner01@student.tgm.ac.at>
2018-06-06 22:46:37 +0200

    First raw code to ForgotpasswordCommand added
    Added raw code for ForgotPasswordCommand, no actuall functionallity

--- Doodler.CLI/Commands/ForgotPasswordCommand.cs
+++ Doodler.CLI/Commands/ForgotPasswordCommand.cs
@@ -1,10 +1,54 @@
-﻿using System;
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Doodler.CLI.Commands
 {
-    class ForgotPasswordCommand
+    [Command(Description = "checks if the user knows his old password", Name = "forgotpassword")]
+    class ForgotPasswordCommand : CommandBase
     {
-    }
+        //new Password
+        String pwuser;
+
+        //User input of Old password
+        String pwcheck;
+
+        //password from database
+        String pwdata;
+
+        public override List<string> CreateArgs()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task<int> OnExecuteAsync(CommandLineApplication app)
+        {
+            try
+            {
+                if (pwcheck.Equals(pwdata))
+                {
+                    //update in database
+                    pwdata = pwuser;
+
+                }
+                else
+                {
+                    throw new Exception();
+                }
+
+                return (Task.FromResult(0));
+            }
+            catch (Exception e)
+            {
+                //User wrote wrong password
+                
+
+
+                return (Task.FromResult(1));
+            }
+      }
+   }
 }
+
