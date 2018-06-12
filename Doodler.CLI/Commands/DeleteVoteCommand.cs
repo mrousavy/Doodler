d0a4cf6f7245b4488f6a3b636c61bd8794f32138
faydin-tgm <faydin@student.tgm.ac.at>
2018-06-12 17:57:35 +0200

    DeleteVoteCommand hinzugefügt

---
+++ Doodler.CLI/Commands/DeleteVoteCommand.cs
@@ -0,0 +1,50 @@
+﻿using DoodlerCore;
+using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doodler.CLI.Commands
+{
+    [Command(Description = "Delete a vote", Name = "unvote")]
+    public class DeleteVoteCommand : CommandBase
+    {
+        public override List<string> CreateArgs()
+        {
+            // TODO: CreateArgs()
+            return new List<string>();
+        }
+
+        protected async override Task<int> OnExecuteAsync(CommandLineApplication app)
+        {
+            try
+            {
+                User user;
+                Vote vote;
+                Poll poll;
+
+                using (var service = Statics.NewService())
+                {
+                    int id = Prompt.GetInt("Poll to delete:");
+                    poll = await service.GetPollByIdAsync(id);
+                    vote = await service.GetVoteFromUserForPoll(Statics.CurrentUser, poll);
+                }
+
+                using (var service = Statics.NewService())
+                {
+                    await service.DeleteVoteAsync(vote);
+                }
+                
+                app.Out.WriteLine($"Successfully deleted Vote!");
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                app.Out.WriteLine($"Could not delete Vote! {ex.Message}");
+                return 1;
+            }
+        }
+
+    }
+}
