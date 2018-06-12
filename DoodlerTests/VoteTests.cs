d1bd3e6fbdd73786add9385d2d4b6e4c48f2a1e6
Dario Bruckner TGM <dbruckner01@student.tgm.ac.at>
2018-06-12 20:28:08 +0200

    VoteTests added
    Added Tests to check if Votes can be read from polls

---
+++ DoodlerTests/VoteTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DoodlerCore;
+using Xunit;
+
+
+namespace DoodlerTests
+{
+    public class VoteTests : IDisposable
+    {
+
+        public User CurrentUser { get; set; }
+
+        public VoteTests()
+        {
+            CurrentUser = Statics.LoginUser().GetAwaiter().GetResult();
+        }
+
+        public void Dispose()
+        {
+        }
+
+        [Fact]
+        public void testfact()
+        {
+            Assert.True(true);
+
+
+        }
+
+        public async Task TestVotes(string title, params string[] answers)
+        {
+            IList<DoodlerCore.Vote> vote;
+
+            Poll poll;
+            // Create poll
+            using (var service = Statics.NewService())
+            {
+
+                var textAnswers = answers.Select(answer => new TextAnswer(poll, answer));
+                poll = await service.CreatePollAsync(CurrentUser, title, DateTime.Now.AddDays(10), textAnswers);
+                await service.SaveAsync();
+            }
+
+
+
+            //Checking Votes
+            using (var service = Statics.NewService())
+            {
+                vote = await service.GetVotesForPollAsync(poll);
+             
+            }
+            Assert.NotEmpty(vote);
+        }
+    }
+}
