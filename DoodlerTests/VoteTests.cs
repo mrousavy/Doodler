d69b9e0de0f47c72aa59d9a9bdb76868bf29bf08
Dario Bruckner TGM <dbruckner01@student.tgm.ac.at>
2018-06-14 21:17:06 +0200

    New Test added
    Added new Test to VoteTests

--- DoodlerTests/VoteTests.cs
+++ DoodlerTests/VoteTests.cs
@@ -37,8 +37,9 @@ namespace DoodlerTests
         /// </summary>
         /// <param name="title"> Title of the poll thats beeing tested </param>
         /// <param name="answers"> String array with the answers </param>
- 
-        
+
+        [Theory]
+        [InlineData("Test: Wann Treffen?", "Heute", "Morgen", "Sonntag")]
         public async Task TestVotes(string title, params string[] answers)
         {
             IList<DoodlerCore.Vote> vote;
@@ -68,5 +69,43 @@ namespace DoodlerTests
             //Checking if the there are votes in the poll
             Assert.NotEmpty(vote);
         }
+
+
+        /// <summary>
+        /// Checks if the the chosen Vote
+        /// </summary>
+        /// <param name="title">title of the poll</param>
+        /// <param name="answers">answers of the poll</param>
+        /// <returns></returns>
+        [Theory]
+        [InlineData("Test: Wann Treffen?", "Heute", "Morgen", "Sonntag")]
+        public async Task TestVotesVote(string title, params string[] answers)
+        {
+            IList<DoodlerCore.Vote> vote;
+            Poll poll;
+            TextAnswer text;
+
+            // Create poll
+            using (var service = Statics.NewService())
+            {
+                var textanswers = answers.Select(answer => new TextAnswer(answer));
+
+                poll = await service.CreatePollAsync(CurrentUser, title, DateTime.Now.AddHours(6), textanswers);
+
+                text = textanswers.ElementAt(1);
+                
+                await service.VoteOnPoll<TextAnswer>(CurrentUser, poll, text);
+
+                vote = await service.GetVotesForPollAsync(poll);
+
+                await service.SaveAsync();
+            }
+
+
+           
+
+            Assert.Same(text, answers.ElementAt(1));
+        }
     }
 }
+
